40b29b0cc9d57604bc1e210c9dce82c342c53041
orpeople <andy460399@gmail.com>
2025-07-21 21:43:07 +0800

    0721 ex1 test

---
+++ STS/00-maven/src/main/java/com/Lombok.java
@@ -0,0 +1,13 @@
+package com;
+
+import com.test.User;
+
+public class Lombok {
+
+	public static void main(String[] args) {
+		User user=new User();
+		
+
+	}
+
+}
---
+++ STS/00-maven/src/main/java/com/test/User.java
@@ -0,0 +1,10 @@
+package com.test;
+
+import lombok.Data;
+
+@Data
+public class User {
+	private Long id;
+	private String name;
+	
+}
